207e9def74d1c2b99373ee1481c3d019b99d336e
moncapiten <moncapiten@gmail.com>
2025-05-15 11:33:00 +0200

    done the first doppler measurement

--- Code/Python/dopplerAnalysis.py
+++ Code/Python/dopplerAnalysis.py
@@ -6,19 +6,14 @@ import pastamarkers
 
 from scipy.optimize import curve_fit
 
-#path = '../../Data/doppler_20250509_102751/'
+
+
+
+
+#path = '../../Data/doppler_20250513_123343/'
 #filename = 'dopplerShift-0-80000'
-#path1 = '../../Data/doppler_20250513_090948/'
-#filename1 = 'dopplerShift-0-0'
-#path = '../../Data/doppler_20250513_091749/'
-#filename = 'dopplerShift-0-80000'
-#numFiles = 6
-#numFiles1 = 8
-
-
-
-path = '../../Data/doppler_20250513_123343/'
-filename = 'dopplerShift-0-80000'
+path = '../../Data/dopplerWithAngle_20250515_112543/'
+filename = 'dopplerShift_0-0-80000'
 numFiles = 20
 
 movingtt, _, movingrx = [], [], []
@@ -34,16 +29,6 @@ for i in range(0, numFiles):
     movingrx = movingrx + _rx.tolist()
 
 
-#movingnewtt = []
-#movingshift = 0
-#for i in range(len(movingtt)):
-
-#    movingnewtt.append( ( movingtt[i] ) + movingshift )
-
-#    if( i != len(movingtt)-1 and movingtt[i] > 0 and movingtt[i+1] < 0):
-#        movingshift = movingnewtt[-1] - movingtt[i+1]
-
-#movingtt = movingnewtt
 
 
 
@@ -81,6 +66,7 @@ movingfrx /= np.max(movingfrx)
 
 path1 = '../../Data/doppler_20250513_121050/'
 filename1 = 'dopplerShift-0-0'
+
 numFiles1 = 20
 
 stoppedtt, _, stoppedrx = [], [], []
@@ -96,15 +82,6 @@ for i in range(0, numFiles1):
     stoppedrx = stoppedrx + _rx.tolist()
 
 
-#stoppednewtt = []
-#stoppedshift = 0
-#for i in range(len(stoppedtt)):
-#    stoppednewtt.append( ( stoppedtt[i] ) + stoppedshift )
-
-#    if( i != len(stoppedtt)-1 and stoppedtt[i] > 0 and stoppedtt[i+1] < 0):
-#        stoppedshift = stoppednewtt[-1] - stoppedtt[i+1]
-
-#stoppedtt = stoppednewtt
 
 
 
@@ -118,7 +95,6 @@ stoppedfrx = np.abs(np.fft.rfft(stoppedrx))
 
 stoppedfrx /= np.max(stoppedfrx)
 
-#print(np.max(ftx), np.max(frx))
 
 
 
@@ -180,8 +156,11 @@ from pastamarkers import pasta
 
 # plot the data
 plt.figure()
-plt.scatter(stoppedfreqs, stoppedfrx, marker = pasta.tortellini, s=1000, label='stoppedrx', linewidth=0.1)
-plt.scatter(movingfreqs, movingfrx, marker = pasta.farfalle, s=1000, label='stoppedrx', linewidth=0.1)
+#plt.scatter(stoppedfreqs, stoppedfrx, marker = pasta.tortellini, s=1000, label='stoppedrx', linewidth=0.1)
+#plt.scatter(movingfreqs, movingfrx, marker = pasta.farfalle, s=1000, label='stoppedrx', linewidth=0.1)
+plt.scatter(stoppedfreqs, stoppedfrx, label='stoppedrx')
+plt.scatter(movingfreqs, movingfrx, label='movingrx')
+
 
 #plt.xlim(1, 5e4)
 #plt.xscale('log')
@@ -198,42 +177,6 @@ plt.ylabel('Amplitude (normalized) [V/Hz]')
 plt.grid(which='both', linestyle='-.', linewidth=0.5)
 
 plt.show()
-
-
-
-
-
-
-
-
-
-#from scipy.signal import correlate
-#def calcola_sfasamento(ch1, ch2):
-#    # Calcolare la correlazione incrociata
-#    correlation = correlate(ch1, ch2, mode='full')
-#    sfasamento = (np.argmax(correlation) - len(ch1) + 1)*10**(3-int(np.floor(np.log10(np.abs(5e5)))))
-#    return sfasamento, correlation
-
-
-
-
-#smallwindowstopped = [ i for i in stoppedfreqs if i > 39.9e3 and i < 40.1e3 ]
-#smallwindowmoving = [ i for i in movingfreqs if i > 39.9e3 and i < 40.1e3 ]
-
-#print( calcola_sfasamento(smallwindowmoving, smallwindowstopped) )
-
-
-#calcola_sfasamento(movingrx, stoppedrx)
-
-
-
-
-
-
-
-
-
-
 
 
 

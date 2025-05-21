525191967619d163563f0a7f6118d524c6a3a618
moncapiten <moncapiten@gmail.com>
2025-05-21 12:24:08 +0200

    we can see all the doppler togethers

--- Code/Python/dopplerAnalysis.py
+++ Code/Python/dopplerAnalysis.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use('TKAgg')  # Use a non-interactive backend for matplotlib
 import matplotlib.pyplot as plt
 import pastamarkers
+from pastamarkers import pasta
 
 from scipy.optimize import curve_fit
 
@@ -32,52 +33,60 @@ highth = 40970
 plt.figure()
 
 
+paths = [ '../../Data/doppler_20250513_121050/',  '../../Data/dopplerWithAngle_20250515_112543/', '../../Data/dopplerWithAngle_20250515_112543/', '../../Data/dopplerWithAngle_20250515_112543/', '../../Data/dopplerWithAngle_20250515_112543/' ]
+filenames = [ 'dopplerShift-0-0', 'dopplerShift_0-0-80000', 'dopplerShift_66-0-80000', 'dopplerShift_133-0-80000', 'dopplerShift_200-0-80000' ]
 
+names = [ 'Stationary transmitter', '0° inclination', '29.7° inclination', '59.8° inclination', '90° inclination' ]
+pastas = [ pasta.tortellini, pasta.farfalle, pasta.fusilli2, pasta.rigatoni, pasta.penne ]
 
 
 
+for j in range(0, len(paths)):
+    print(j)
+    path1 = paths[j]
+    filename1 = filenames[j]
 
-'''
+    numFiles1 = 20
 
-tt, _, rx = [], [], []
+    tt, _, rx = [], [], []
 
-for i in range(0, numFiles):
-    specificFilename = filename + '_' + str(i) + '.txt'
-    print(specificFilename)
-    # Load the data from the file
-    # The data is assumed to be in three columns: time, tx, rx  
-    _tt, _tx, _rx = np.loadtxt(path+specificFilename, unpack=True)
-    tt = tt + _tt.tolist()
-#    tx = tx + _tx.tolist()
-    rx = rx + _rx.tolist()
+    for i in range(0, numFiles1):
+        specificFilename = filename1 + '_' + str(i) + '.txt'
+        print(specificFilename)
+        # Load the data from the file
+        # The data is assumed to be in three columns: time, tx, rx  
+        _tt, _tx, _rx = np.loadtxt(path1+specificFilename, unpack=True)
+        tt = tt + _tt.tolist()
+    #    tx = tx + _tx.tolist()
+        rx = rx + _rx.tolist()
 
 
 
 
 
+    # fourier transform
+    freqs = np.fft.rfftfreq(len(tt), np.mean(np.diff(tt)))
+    frx = np.abs(np.fft.rfft(rx))
 
+    frx /= np.max(frx)
 
+    tt, _, rx = [], [], []
 
+    # mask the data
+    mask = (freqs > lowth) & (freqs < highth)
+    freqs = freqs[mask]
+    frx = frx[mask]
 
-# fourier transform
-freqs = np.fft.rfftfreq(len(tt), np.mean(np.diff(tt)))
-frx = np.abs(np.fft.rfft(rx))
+#    plt.scatter(freqs, frx, label=names[j])
+    plt.scatter(freqs, frx, marker = pastas[j], s=500, label=names[j], linewidth=0.1)
 
-frx /= np.max(frx)
 
-mask = (freqs > lowth) & (freqs < highth)
-freqs = freqs[mask]
-frx = frx[mask]
 
 
-#print(np.max(ftx), np.max(frx))
 
-# plot the data
 
-tt, _, rx = [], [], []
 
 
-plt.scatter(freqs, frx, label='moving')
 
 
 
@@ -89,44 +98,20 @@ plt.scatter(freqs, frx, label='moving')
 
 
 
-'''
 
 
-path1 = '../../Data/doppler_20250513_121050/'
-filename1 = 'dopplerShift-0-0'
 
-numFiles1 = 20
 
-tt, _, rx = [], [], []
 
-for i in range(0, numFiles1):
-    specificFilename = filename1 + '_' + str(i) + '.txt'
-    print(specificFilename)
-    # Load the data from the file
-    # The data is assumed to be in three columns: time, tx, rx  
-    _tt, _tx, _rx = np.loadtxt(path1+specificFilename, unpack=True)
-    tt = tt + _tt.tolist()
-#    tx = tx + _tx.tolist()
-    rx = rx + _rx.tolist()
 
 
 
 
 
-# fourier transform
-freqs = np.fft.rfftfreq(len(tt), np.mean(np.diff(tt)))
-frx = np.abs(np.fft.rfft(rx))
 
-frx /= np.max(frx)
 
-tt, _, rx = [], [], []
 
-# mask the data
-mask = (freqs > lowth) & (freqs < highth)
-freqs = freqs[mask]
-frx = frx[mask]
 
-plt.scatter(freqs, frx, label='Stationary transmitter')
 
 
 
@@ -150,251 +135,7 @@ plt.scatter(freqs, frx, label='Stationary transmitter')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-path1 = '../../Data/dopplerWithAngle_20250515_112543/'
-filename1 = 'dopplerShift_0-0-80000'
-
-numFiles1 = 20
-
-tt, _, rx = [], [], []
-
-for i in range(0, numFiles1):
-    specificFilename = filename1 + '_' + str(i) + '.txt'
-    print(specificFilename)
-    # Load the data from the file
-    # The data is assumed to be in three columns: time, tx, rx  
-    _tt, _tx, _rx = np.loadtxt(path1+specificFilename, unpack=True)
-    tt = tt + _tt.tolist()
-#    tx = tx + _tx.tolist()
-    rx = rx + _rx.tolist()
-
-
-
-
-
-# fourier transform
-freqs = np.fft.rfftfreq(len(tt), np.mean(np.diff(tt)))
-frx = np.abs(np.fft.rfft(rx))
-
-frx /= np.max(frx)
-
-tt, _, rx = [], [], []
-
-# mask the data
-mask = (freqs > lowth) & (freqs < highth)
-freqs = freqs[mask]
-frx = frx[mask]
-
-plt.scatter(freqs, frx, label='0° inclination')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-path1 = '../../Data/dopplerWithAngle_20250515_112543/'
-filename1 = 'dopplerShift_66-0-80000'
-
-numFiles1 = 20
-
-tt, _, rx = [], [], []
-
-for i in range(0, numFiles1):
-    specificFilename = filename1 + '_' + str(i) + '.txt'
-    print(specificFilename)
-    # Load the data from the file
-    # The data is assumed to be in three columns: time, tx, rx  
-    _tt, _tx, _rx = np.loadtxt(path1+specificFilename, unpack=True)
-    tt = tt + _tt.tolist()
-#    tx = tx + _tx.tolist()
-    rx = rx + _rx.tolist()
-
-
-
-
-
-# fourier transform
-freqs = np.fft.rfftfreq(len(tt), np.mean(np.diff(tt)))
-frx = np.abs(np.fft.rfft(rx))
-
-frx /= np.max(frx)
-
-tt, _, rx = [], [], []
-
-# mask the data
-mask = (freqs > lowth) & (freqs < highth)
-freqs = freqs[mask]
-frx = frx[mask]
-# 200:90=60:x
-plt.scatter(freqs, frx, label='29.7° inclination')
-
-
-
-
-
-
-
-
-
-
-
-
-path1 = '../../Data/dopplerWithAngle_20250515_112543/'
-filename1 = 'dopplerShift_133-0-80000'
-
-numFiles1 = 20
-
-tt, _, rx = [], [], []
-
-for i in range(0, numFiles1):
-    specificFilename = filename1 + '_' + str(i) + '.txt'
-    print(specificFilename)
-    # Load the data from the file
-    # The data is assumed to be in three columns: time, tx, rx  
-    _tt, _tx, _rx = np.loadtxt(path1+specificFilename, unpack=True)
-    tt = tt + _tt.tolist()
-#    tx = tx + _tx.tolist()
-    rx = rx + _rx.tolist()
-
-
-
-
-
-# fourier transform
-freqs = np.fft.rfftfreq(len(tt), np.mean(np.diff(tt)))
-frx = np.abs(np.fft.rfft(rx))
-
-frx /= np.max(frx)
-
-tt, _, rx = [], [], []
-
-# mask the data
-mask = (freqs > lowth) & (freqs < highth)
-freqs = freqs[mask]
-frx = frx[mask]
-
-plt.scatter(freqs, frx, label='59.8° inclination')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-path1 = '../../Data/dopplerWithAngle_20250515_112543/'
-filename1 = 'dopplerShift_200-0-80000'
-
-numFiles1 = 20
-
-tt, _, rx = [], [], []
-
-for i in range(0, numFiles1):
-    specificFilename = filename1 + '_' + str(i) + '.txt'
-    print(specificFilename)
-    # Load the data from the file
-    # The data is assumed to be in three columns: time, tx, rx  
-    _tt, _tx, _rx = np.loadtxt(path1+specificFilename, unpack=True)
-    tt = tt + _tt.tolist()
-#    tx = tx + _tx.tolist()
-    rx = rx + _rx.tolist()
-
-
-
-
-
-# fourier transform
-freqs = np.fft.rfftfreq(len(tt), np.mean(np.diff(tt)))
-frx = np.abs(np.fft.rfft(rx))
-
-frx /= np.max(frx)
-
-tt, _, rx = [], [], []
-
-# mask the data
-mask = (freqs > lowth) & (freqs < highth)
-freqs = freqs[mask]
-frx = frx[mask]
-
-plt.scatter(freqs, frx, label='90° inclination')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from pastamarkers import pasta
+#from pastamarkers import pasta
 
 # plot the data
 #plt.figure()
@@ -414,7 +155,7 @@ plt.title('Fourier Transform of the received signals')
 
 #plt.yscale('log')
 plt.xlabel('Frequency [Hz]')
-plt.ylabel('Amplitude (normalized) [V/Hz]')
+plt.ylabel('Amplitude (normalized) [pure]')
 
 plt.grid(which='both', linestyle='-.', linewidth=0.5)
 
